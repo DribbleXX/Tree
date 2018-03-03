784d28d94175c63b94ef334b58a6be7ae780f927
Stipendi <turvatili@icloud.com>
2018-03-03 22:05:16 +0000

    Fixed a bug

--- structures/game.js
+++ structures/game.js
@@ -14,12 +14,12 @@ module.exports = class Game {
   static async create(game) {
     if (!index.tree.games.get(game.channel.id))
       index.tree.games.set(game.channel.id, []);
-    if (index.tree.games.get(game.channel.id).filter(g => g.name === game.name).length) {
+    if (index.tree.games.get(game.channel.id).filter(g => g.name === game.name).length)
       msg.create(new Embed("Game wasn't created", "There is already a game of " + string.capitalize(game.name, true) + " in that channel"), game.channel);
-      return this.remove(game);
+    else{
+      index.tree.games.get(game.channel.id).push(game);
+      msg.create(new Embed(user.discrim(game.host) + " started a game of **" + string.capitalize(game.name, true) + "**", "Type `" + (await index.tree.getPrefix(game.channel.guild.id)) + "join` to join!"), game.channel);
     }
-    index.tree.games.get(game.channel.id).push(game);
-    msg.create(new Embed(user.discrim(game.host) + " started a game of **" + string.capitalize(game.name, true) + "**", "Type `" + (await index.tree.getPrefix(game.channel.guild.id)) + "join` to join!"), game.channel);
   }
   static remove(game) {
     index.tree.games.get(game.channel.id).splice(index.tree.games.get(game.channel.id).indexOf(game), 1);
