36e83641710ddcd9e84973fa25d5e12daf1239aa
Stipendi <turvatili@icloud.com>
2018-03-04 10:16:15 +0000

    More user-friendly (Thansk Max)

--- structures/tictactoe.js
+++ structures/tictactoe.js
@@ -20,10 +20,14 @@ module.exports = class Tictactoe extends Game {
     }else{
       this.turn = this.players.filter(p => p.id !== this.turn.id)[0];
     }
-    let embed = new Embed("Tic Tac Toe", "It's **" + this.turn.username + "'s turn!**\n```\n_____________\n|_" + this.grid[0][0] + "_|_" + this.grid[0][1] + "_|_" + this.grid[0][2] + "_|\n|_" + this.grid[1][0] + "_|_" + this.grid[1][1] + "_|_" + this.grid[1][2] + "_|\n|_" + this.grid[2][0] + "_|_" + this.grid[2][1] + "_|_" + this.grid[2][2] + "_|```");
-    embed.addFooter("X - " + this.players.filter(p => p.side === "x")[0].username + " | O - " + this.players.filter(p => p.side === "o")[0].username);
-    if (this.state === 1)
+    let embed;
+    if (this.state === 1) {
+      embed = new Embed("Tic Tac Toe", "**" + this.turn.username + " gets to go first!**\n```\n________________\n|_A1_|_B1_|_C1_|\n|_A2_|_B2_|_C2_|\n|_A3_|_B3_|_C3_|```");
       embed.addField("How To Play", "Every **column** represents a **letter** A-C and every **row** represents a **number** 1-3. To make a play on a box, take the letter of that column and the number of that row and send it in chat. For example if I wanted to play on the first column on the second row I'd type `A2`. You can only play on your turn.");
+    }else{
+      embed = new Embed("Tic Tac Toe", "It's **" + this.turn.username + "'s turn!**\n```\n_____________\n|_" + this.grid[0][0] + "_|_" + this.grid[0][1] + "_|_" + this.grid[0][2] + "_|\n|_" + this.grid[1][0] + "_|_" + this.grid[1][1] + "_|_" + this.grid[1][2] + "_|\n|_" + this.grid[2][0] + "_|_" + this.grid[2][1] + "_|_" + this.grid[2][2] + "_|```");
+    }
+    embed.addFooter("X - " + this.players.filter(p => p.side === "x")[0].username + " | O - " + this.players.filter(p => p.side === "o")[0].username);
     if (this.state === 2 && (await this.channel.getMessages(4)).filter(msg => msg.id === this.lastMessage.id).length)
       this.lastMessage.edit({ embed: embed, content: "" });
     else
